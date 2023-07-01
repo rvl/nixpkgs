28baea5936dd2aff5e829ffe3c96958962166fde
sternenseemann <sternenseemann@systemli.org>
2023-07-01 11:52:03 +0200

    haskell.packages.ghc94: remove remaining manually chosen versions
    * `ghc-lib*` is tracked by Stackage LTS 21 and has the matching versions
      for GHC 9.4.
    
    * `primitive` is actually upgraded in LTS 21 which we should follow.
    
    * `th-desugar` and `th-abstraction` are downgraded in LTS 21 which we
      should follow.
    
    * `rope-utf16-splay` matches the manually chosen version.

--- pkgs/development/haskell-modules/hackage-packages.nix
+++ pkgs/development/haskell-modules/hackage-packages.nix
@@ -232785,33 +232785,6 @@ self: {
        license = lib.licenses.mit;
      }) {inherit (pkgs) primesieve;};
 
-  "primitive_0_7_4_0" = callPackage
-    ({ mkDerivation, base, base-orphans, deepseq, ghc-prim, QuickCheck
-     , quickcheck-classes-base, tagged, tasty, tasty-bench
-     , tasty-quickcheck, template-haskell, transformers
-     , transformers-compat
-     }:
-     mkDerivation {
-       pname = "primitive";
-       version = "0.7.4.0";
-       sha256 = "1mddh42i6xg02z315c4lg3zsxlr3wziwnpzh2nhzdcifh716sbav";
-       revision = "1";
-       editedCabalFile = "0av20kv9ib795qr62yzby5l46vhkifzc6fdj8cppzsfwnfbyvw62";
-       libraryHaskellDepends = [
-         base deepseq template-haskell transformers
-       ];
-       testHaskellDepends = [
-         base base-orphans ghc-prim QuickCheck quickcheck-classes-base
-         tagged tasty tasty-quickcheck transformers transformers-compat
-       ];
-       benchmarkHaskellDepends = [
-         base deepseq tasty-bench transformers
-       ];
-       description = "Primitive memory-related operations";
-       license = lib.licenses.bsd3;
-       hydraPlatforms = lib.platforms.none;
-     }) {};
-
   "primitive" = callPackage
     ({ mkDerivation, base, base-orphans, deepseq, ghc-prim, QuickCheck
      , quickcheck-classes-base, tagged, tasty, tasty-bench
